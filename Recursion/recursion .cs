35e4530c0ccd2022a26428be05779c9664fdc4b7
ckaul <ckaul@PC>
2024-06-21 20:38:23 +0700

    Classwork

---
+++ Recursion/recursion .cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Введите номер этажа");
+            //int floor=Convert.ToInt32(Console.ReadLine());
+            //Elevator(floor);
+
+
+            Console.WriteLine("Введите число ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{n}!={Factorial(n)}");
+
+        }
+
+        static int Factorial(int n) 
+        {
+            if (n==0)
+            {
+                
+                return 1;
+
+            } 
+            else return n*Factorial(n-1);
+        
+        }
+        static void Function()
+        {
+            Console.WriteLine("PRIVET");
+            Console.WriteLine("YA RABOTAU");
+
+
+        }
+         
+        static void Elevator(int floor)
+        {
+
+            if (floor == 0) 
+            {
+                Console.WriteLine($"Вы в подвале ");
+
+                return; 
+            }
+
+            Console.WriteLine($"Вы на {floor} этаже" );
+            Elevator(floor-1);
+            Console.WriteLine($"Вы на {floor} этаже");
+
+        }
+    }
+}
